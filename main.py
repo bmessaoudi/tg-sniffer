ef0be5f9061781d69c473d0ede16c56e3ad44e43
bmessaoudi <bilelmessaudi@gmail.com>
2024-03-14 16:29:59 +0100

    fixed async function

--- main.py
+++ main.py
@@ -11,21 +11,30 @@ client = TelegramClient('main', API_ID, API_HASH)
 chat_ids = []
 chat_names = ['FXpro: Delta', 'Oblivion']
 
-client.start()
 
+async def main():
+    print('Bot started')
+    await client.start()
 
-for name in chat_names:
-    result = client(functions.contacts.SearchRequest(q=name, limit=1))
-    if result.my_results and result.my_results[0].channel_id:
-        chat_ids.append(result.my_results[0].channel_id)
+    for name in chat_names:
+        print('Search chat id of: ' + name)
+        result = await client(functions.contacts.SearchRequest(q=name, limit=1))
+        if result.my_results and result.my_results[0].channel_id:
+            print('Founded: ' + str(result.my_results[0].channel_id))
+            chat_ids.append(result.my_results[0].channel_id)
+        else:
+            print('Not founded')
 
+    @events.register(events.NewMessage(chats=[chat_ids]))
+    async def sniffer(event):
+        print('MESSAGE EVENT: ' + str(event))
+        message_text = event.message.message
+        await client.send_message(entity=-1001912225575, message=message_text)
 
-@events.register(events.NewMessage(chats=[chat_ids]))
-async def sniffer(event):
-    print(event)
-    message_text = event.message.message
-    await client.send_message(entity=-1001912225575, message=message_text)
+    client.add_event_handler(sniffer)
 
+    print('Start sniffing')
 
-client.add_event_handler(sniffer)
-client.run_until_disconnected()
+    await client.run_until_disconnected()
+
+client.loop.run_until_complete(main())
