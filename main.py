5bcf1381f2820fabbb48ae146a11176b8b1cece4
bmessaoudi <bilelmessaudi@gmail.com>
2024-03-15 13:59:28 +0100

    add logging and verbose stdout

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from telethon import TelegramClient, functions, events
 from telethon.sessions import StringSession
 import os
 from dotenv import load_dotenv
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s %(levelname)s %(message)s",
+                    handlers=[
+                        logging.FileHandler("output.log"),
+                        logging.StreamHandler()
+                    ])
 
 load_dotenv()
 
@@ -12,38 +20,39 @@ TELEGRAM_STRING_SESSION = os.getenv('TELEGRAM_STRING_SESSION')
 client = TelegramClient(StringSession(
     TELEGRAM_STRING_SESSION), API_ID, API_HASH)
 chat_ids = []
-chat_names = ['FXpro: Delta', 'Oblivion']
-channel_destination = 'IGENIUS 15 DAYS TRIAL'
+# chat_names = ['FXpro: Delta', 'Oblivion']
+chat_names = ['Test crypto botty']
+chat_destination = 'IGENIUS 15 DAYS TRIAL'
 
 
 async def main():
-    print('Bot started')
-    await client.start()
+    global chat_id_destination
+    try:
+        logging.info('Bot started')
+        await client.start()
 
-    for name in chat_names:
-        print('Search chat id of: ' + name)
-        result = await client(functions.contacts.SearchRequest(q=name, limit=1))
-        if result.my_results and result.my_results[0].channel_id:
-            print('Founded: ' + str(result.my_results[0].channel_id))
-            chat_ids.append(result.my_results[0].channel_id)
-        else:
-            print('Not founded')
+        for name in chat_names:
+            logging.info('Search chat ID of: ' + name)
+            result = await client(functions.contacts.SearchRequest(q=name, limit=1))
+            if result.my_results and result.my_results[0].channel_id:
+                logging.info(
+                    'Founded: ' + str(result.my_results[0].channel_id))
+                chat_ids.append(result.my_results[0].channel_id)
+            else:
+                logging.warning('Not founded')
 
-    chat_id_destination = 1001912225575
-    result = await client(functions.contacts.SearchRequest(q=channel_destination, limit=1))
-    if result.my_results and result.my_results[0].channel_id:
-        chat_id_destination = result.my_results[0].channel_id
+        @events.register(events.NewMessage(chats=chat_ids))
+        async def sniffer(event):
+            logging.info('MESSAGE EVENT: ' + str(event))
+            message_text = event.message.message
+            await client.send_message(entity=chat_destination, message=message_text)
 
-    @events.register(events.NewMessage(chats=[chat_ids]))
-    async def sniffer(event):
-        print('MESSAGE EVENT: ' + str(event))
-        message_text = event.message.message
-        await client.send_message(entity=chat_id_destination, message=message_text)
+        client.add_event_handler(sniffer)
 
-    client.add_event_handler(sniffer)
+        logging.info('Start sniffing')
 
-    print('Start sniffing')
-
-    await client.run_until_disconnected()
+        await client.run_until_disconnected()
+    except Exception as e:
+        logging.error(e)
 
 client.loop.run_until_complete(main())
